a28cfa5d9e87ec261648046726bd0d770a8e3c57
JaskerX <jaskerlp@gmx.de>
2025-12-02 00:30:17 +0100

    add notification and progressBar

--- timerHandler.js
+++ timerHandler.js
@@ -1,14 +1,18 @@
+const { Notification } = require("electron");
+
 class TimerHandler {
 
     #intervalId = null;
-    #seconds = 0;
+    #remainingSeconds = 0;
+    #totalSeconds = 0;
 
     reset (seconds) {
         if (this.#intervalId !== null) {
             this.pause();
         }
 
-        this.#seconds = seconds;
+        this.#totalSeconds = seconds;
+        this.#remainingSeconds = seconds;
     }
 
     start (window) {
@@ -17,13 +21,21 @@ class TimerHandler {
         }
 
         this.#intervalId = setInterval(async () => {
-            if (this.#seconds === 0) {
+            if (this.#remainingSeconds === 0) {
                 this.pause();
+                new Notification({
+                    title: "Timer abgelaufen",
+                    body: "Der Timer ist abgelaufen."
+                }).show();
+                // remove progressBar
+                window.setProgressBar(-1);
                 return;
             }
 
-            this.#seconds -= 1;
-            window.webContents.send("update-timer", this.#seconds);
+            this.#remainingSeconds -= 1;
+
+            window.webContents.send("update-timer", this.#remainingSeconds);
+            window.setProgressBar(1 - (this.#remainingSeconds / this.#totalSeconds));
         }, 1000);
     }
 
